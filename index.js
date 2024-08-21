8a9bea52dc57c51e00a87b45a4cfd68386376c03
Abhinav Choudhary <abhinav@Abhinavs-Air.Dlink>
2024-08-21 18:21:43 +0530

    one

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 4000; // Use the port from .env, or default to 4000
+const PORT = process.env.PORT  // Use the port from .env, or default to 4000
 
 // Middleware to parse JSON requests
 app.use(express.json({ limit: '100mb' }));

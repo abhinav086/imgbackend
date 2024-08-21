e168ffc55fcd817d35c98c4cc58918546aedb218
Abhinav Choudhary <abhinav@Abhinavs-MacBook-Air.local>
2024-08-21 17:41:10 +0530

    one

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ import cors from "cors";
 
 const app = express();
 const corsOptions = {
-    origin: ['http://localhost:3000','https://my-image-app-ruby.vercel.app'],
+    origin: ['http://localhost:3000', 'https://my-image-app-ruby.vercel.app'],
     credentials: true
 };
 app.use(cors(corsOptions));
-
-const PORT = process.env.PORT || 4000; // Use the port from .env, or default to 4000
 
 // Middleware to parse JSON requests
 app.use(express.json({ limit: '100mb' }));
@@ -21,7 +19,7 @@ connectDB('mongodb+srv://abhinavchoudharyofficial:abhi123@cluster0.qy14qdv.mongo
 // Use the image routes from `imageRoutes.js`
 app.use('/api', imageRoutes); // Mount all image-related routes under the '/api' path
 
-// Start the Express server
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
+// Start the Express server on the dynamic port from the environment variable
+app.listen(process.env.PORT, () => {
+    console.log(`Server started`);
 });
